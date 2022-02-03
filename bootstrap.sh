6bdba5e2b06389b3a7f39dad7e5eb21c6e553a04
Sergiy Markin <serge.markin@wepanow.com>
2022-02-03 17:51:06 -0600

    tested - airship 1.0

---
+++ bootstrap.sh
@@ -0,0 +1,57 @@
+#!/bin/bash 
+
+      set -ex
+
+      
+      mkdir -p /root/deploy && cd "$_"
+      git config --global http.sslVerify false # Do NOT do this!
+      apt update
+      apt install git curl make -y
+      rm -f /usr/lib/python3/dist-packages/PyYAML-3.12.egg-info
+      apt install python3-pip python3-setuptools -y
+      pip3 install PyYAML
+      apt install python-pip python-setuptools -y
+      apt install python-cmd2 python-openstackclient python-heatclient -y
+      git clone https://opendev.org/airship/treasuremap/
+      cd /root/deploy/treasuremap/
+      git checkout v1.8-prime
+      ./tools/deployment/airskiff/developer/000-clone-dependencies.sh
+      ./tools/deployment/airskiff/developer/009-setup-apparmor.sh
+      sleep 5
+      cd ../openstack-helm-infra/
+      cp ../openstack-helm-infra.diff ./
+      git apply openstack-helm-infra.diff
+      cd ../treasuremap/
+      ./tools/deployment/airskiff/developer/020-setup-client.sh
+      ./tools/deployment/airskiff/developer/010-deploy-k8s.sh
+      chmod 777 ~/.kube/config
+      cp /etc/resolv.conf /etc/resolv.conf.backup
+      sudo bash -c 'echo '\''nameserver 10.96.0.10'\'' > /etc/resolv.conf'
+      sudo bash -c 'echo '\''nameserver 8.8.8.8'\'' >> /etc/resolv.conf'
+      sudo bash -c 'echo '\''nameserver 8.8.4.4'\'' >> /etc/resolv.conf'
+      sudo bash -c 'echo '\''search svc.cluster.local cluster.local'\'' >> /etc/resolv.conf'
+      sudo bash -c 'echo '\''options ndots:5 timeout:1 attempts:1'\'' >> /etc/resolv.conf'
+      sudo rm /etc/resolv.conf.backup
+      cd ../armada
+      git checkout a1b693b84d97f2cd0e0f9482d2f85a894c45c265
+      cp ../armada.diff ./
+      git apply armada.diff
+      make charts
+      pip install tox
+      tox -e genconfig
+      tox -e genpolicy
+      make images
+      sleep 5
+      cd ../treasuremap/
+      git checkout 1678cf635fe7ae7130c0dbef8dfb5494b8c69c16
+      cp ../treasuremap.diff ./
+      git apply treasuremap.diff
+      ./tools/deployment/airskiff/developer/030-armada-bootstrap.sh
+      export OS_CLOUD=airship
+      openstack endpoint list
+      openstack service list
+      cd ../shipyard
+      git checkout 30f3a989c7
+      cd ../treasuremap
+      ./tools/deployment/airskiff/developer/100-deploy-osh.sh
+      #kubectl apply -f https://k8s.io/examples/admin/dns/dnsutils.yaml
---
+++ cleanup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -ex
+
+rm -rf armada
+rm -rf deckhand/
+rm -rf openstack-helm
+rm -rf openstack-helm-infra
+rm -rf pegleg/
+rm -rf shipyard/
+rm -rf treasuremap
+
