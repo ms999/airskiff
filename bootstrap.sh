ff7cfd58b7bc342d3b27303f3f244a9112cd568e
Sergiy Markin <serge.markin@wepanow.com>
2022-02-09 00:26:18 -0600

    move to devcloud

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,23 +1,26 @@
 #!/bin/bash 
 
       set -ex
-
-      
-      mkdir -p /root/deploy && cd "$_"
-      ./cleanup.sh
+      echo DefaultTasksMax=1000000 | sudo tee /etc/systemd/system.conf
+      sudo systemctl daemon-reload
+      sudo systemctl daemon-reexec
+      sudo service systemd-logind restart
+      sudo systemctl stop apparmor
+      sudo systemctl disable apparmor
+      sudo apt remove --assume-yes --purge apparmor
       git config --global http.sslVerify false # Do NOT do this!
-      apt update
-      apt install git curl make -y
-      rm -f /usr/lib/python3/dist-packages/PyYAML-3.12.egg-info
-      apt install python3-pip python3-setuptools -y
+      sudo apt update
+      sudo apt install git curl make -y
+      sudo rm -f /usr/lib/python3/dist-packages/PyYAML-3.12.egg-info
+      sudo apt install python3-pip python3-setuptools -y
       pip3 install PyYAML
-      apt install python-pip python-setuptools -y
-      apt install python-cmd2 python-openstackclient python-heatclient -y
+      sudo apt install python-pip python-setuptools -y
+      sudo apt install python-cmd2 python-openstackclient python-heatclient -y
       git clone https://opendev.org/airship/treasuremap/
-      cd /root/deploy/treasuremap/
+      cd ./treasuremap/
       git checkout v1.8-prime
       ./tools/deployment/airskiff/developer/000-clone-dependencies.sh
-      ./tools/deployment/airskiff/developer/009-setup-apparmor.sh
+      # ./tools/deployment/airskiff/developer/009-setup-apparmor.sh
       sleep 5
       cd ../openstack-helm-infra/
       cp ../openstack-helm-infra.diff ./
@@ -25,8 +28,12 @@
       cd ../treasuremap/
       ./tools/deployment/airskiff/developer/020-setup-client.sh
       ./tools/deployment/airskiff/developer/010-deploy-k8s.sh
+      sudo systemctl stop apparmor
+      sudo systemctl disable apparmor
+      sudo apt remove --assume-yes --purge apparmor      
       chmod 777 ~/.kube/config
-      cp /etc/resolv.conf /etc/resolv.conf.backup
+
+      sudo cp /etc/resolv.conf /etc/resolv.conf.backup
       sudo bash -c 'echo '\''nameserver 10.96.0.10'\'' > /etc/resolv.conf'
       sudo bash -c 'echo '\''nameserver 8.8.8.8'\'' >> /etc/resolv.conf'
       sudo bash -c 'echo '\''nameserver 8.8.4.4'\'' >> /etc/resolv.conf'
@@ -54,5 +61,5 @@
       cd ../shipyard
       git checkout 30f3a989c7
       cd ../treasuremap
-      ./tools/deployment/airskiff/developer/100-deploy-osh.sh
+      # ./tools/deployment/airskiff/developer/100-deploy-osh.sh
       #kubectl apply -f https://k8s.io/examples/admin/dns/dnsutils.yaml
