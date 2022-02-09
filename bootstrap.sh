52c3827b1733fc8b5f5f3603b2070c92a5e6943e
Sergiy Markin <serge.markin@wepanow.com>
2022-02-09 09:53:18 -0600

    +1

--- bootstrap.sh
+++ bootstrap.sh
@@ -13,7 +13,7 @@
       sudo apt install git curl make -y
       sudo rm -f /usr/lib/python3/dist-packages/PyYAML-3.12.egg-info
       sudo apt install python3-pip python3-setuptools -y
-      pip3 install PyYAML
+      sudo pip3 install PyYAML
       sudo apt install python-pip python-setuptools -y
       sudo apt install python-cmd2 python-openstackclient python-heatclient -y
       git clone https://opendev.org/airship/treasuremap/
@@ -26,8 +26,8 @@
       cp ../openstack-helm-infra.diff ./
       git apply openstack-helm-infra.diff
       cd ../treasuremap/
-      ./tools/deployment/airskiff/developer/020-setup-client.sh
-      ./tools/deployment/airskiff/developer/010-deploy-k8s.sh
+      sudo ./tools/deployment/airskiff/developer/020-setup-client.sh
+      sudo ./tools/deployment/airskiff/developer/010-deploy-k8s.sh
       sudo systemctl stop apparmor
       sudo systemctl disable apparmor
       sudo apt remove --assume-yes --purge apparmor      
@@ -40,22 +40,22 @@
       sudo bash -c 'echo '\''search svc.cluster.local cluster.local'\'' >> /etc/resolv.conf'
       sudo bash -c 'echo '\''options ndots:5 timeout:1 attempts:1'\'' >> /etc/resolv.conf'
       sudo rm /etc/resolv.conf.backup
-      # cd ../armada
-      # git checkout a1b693b84d97f2cd0e0f9482d2f85a894c45c265
-      # cp ../armada.diff ./
-      # git apply armada.diff
-      # make charts
-      # pip install tox
-      # sleep 5
-      # tox -e genconfig
-      # tox -e genpolicy
-      # sudo make images
-      # sleep 5
+      cd ../armada
+      git checkout a1b693b84d97f2cd0e0f9482d2f85a894c45c265
+      cp ../armada.diff ./
+      git apply armada.diff
+      make charts
+      pip install tox
+      sleep 5
+      tox -e genconfig
+      tox -e genpolicy
+      make images
+      sleep 5
       cd ../treasuremap/
       git checkout 1678cf635fe7ae7130c0dbef8dfb5494b8c69c16
       cp ../treasuremap.diff ./
       git apply treasuremap.diff
-      sudo ./tools/deployment/airskiff/developer/030-armada-bootstrap.sh
+      ./tools/deployment/airskiff/developer/030-armada-bootstrap.sh
       export OS_CLOUD=airship
       openstack endpoint list
       openstack service list
